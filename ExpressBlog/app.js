d598b74029fa5503dea02ca4a04626b612109e35
Minh Tam <minhtam.seta@gmail.com>
2014-08-21 16:51:22 +0700

    host nodeJs on IIS

--- ExpressBlog/app.js
+++ ExpressBlog/app.js
@@ -9,6 +9,7 @@
 	var http = require('http');
 	var path = require('path');
 	var app = express();
+	var ECT = require('ect');	
 
 	application_start();
 	registerRoute();
@@ -25,28 +26,31 @@
 
 	//setup port, views path, view engine
 	function application_start(){		
-
+		var ectRenderer = ECT({ watch: true, root: __dirname + '/views' });
+		app.engine('.ect', ectRenderer.render);
 		// all environments
-		app.set('port', process.env.PORT || 3000);
-		app.set('views', path.join(__dirname, 'views'));
-		app.set('view engine', 'jade');
+		app.set('port', process.env.PORT || 1000);
+		//app.set('views', path.join(__dirname, 'views'));
+		//app.set('view engine', 'jade');
 		app.use(express.compress());
 		app.use(express.favicon());
 		app.use(express.logger('dev'));
 		app.use(express.json());
 		app.use(express.urlencoded());
 		app.use(express.methodOverride());
-		app.use(app.router);
+        app.use(process.env.appvirtdir || '', app.router);//app.use(app.router);
 		app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 		app.use(express.static(path.join(__dirname, 'public')));
 		app.use(function(req, res){
-		    	res.status(404).render('error404.ejs', {url: req.url});
+		    	res.status(404).render('error404.ect', {url: req.url});
 	    });
 	}
 
 	function registerRoute(){
 		app.get('/', routes.index);
 		app.get('/users', user.list);
+		app.get('/displayForm', routes.displayForm);
+		app.post('/postForm', routes.postForm);
 	}
 
 })();
--- ExpressBlog/routes/index.js
+++ ExpressBlog/routes/index.js
@@ -4,5 +4,17 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	var data = { title: 'Express' };
+  	res.render('index.ect', data);
+};
+
+exports.displayForm = function(req, res){
+	res.render('displayForm.ect');
+};
+
+exports.postForm = function(req, res){
+	console.log(req);
+	//res.render('displayForm.ect');
+	var data = { name: req.body.name};
+	res.json(data);
 };
